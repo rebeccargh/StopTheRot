d4d2ee4729e92663c475689fc053e842275d70ce
Rebecca H <hartr693@newschool.edu>
2024-04-04 17:18:24 -0400

    Filters!
    Added filters with basic styling & hover/selected states. JS to filter options based on matching criteria (this is still a little broken)

--- assets/main.js
+++ assets/main.js
@@ -40,9 +40,9 @@ fetch('assets/data.json')
 		// And passes the data to the function, above!
 		renderItems(data)
 	})
-
+	
 	// Slider output
-	var slider = document.getElementById("activitySlider");
+var slider = document.getElementById("activitySlider");
 var output = document.getElementById("sliderValue");
 output.innerHTML = slider.value; // Display the default slider value
 
@@ -51,4 +51,110 @@ slider.oninput = function() {
 	output.innerHTML = (this.value/1).toFixed(0);
 }
 
-// if a value is false, add hidden class on button click - will filter by values?
+
+// Filter button for free
+
+document.getElementById('moneyFilter').onclick = () => {
+	renderBlocks('0');
+	document.getElementById('moneyFilter').classList.toggle('free');
+  };
+  
+  function renderBlocks(costsMoney) {
+	fetch('assets/data.json') 
+	  .then(response => response.json()) // Parse the response as JSON
+	  .then(data => {
+		const container = document.getElementById('data-list');
+		container.innerHTML = ''; // Clear previous content
+	
+		data.forEach((block) => {
+		  if (block.costsMoney === costsMoney) {
+			// Add the block
+			let listItem =
+			  `
+				<li>
+				  <p>${block.emoji}</p>
+				  <h2>${block.itemName}</h2>
+				  <p><em>Do I have to leave the neighborhood?</em></p>
+				  <p>${block.requiresTransit}</p>
+				  <p><em>Activity Rating</em></p>
+				  <p>${block.activityRating} / 10</p>
+				</li>
+			  `;
+	
+			container.insertAdjacentHTML('beforeend', listItem);
+		  }
+		});
+	  })
+  }
+  
+  // Filter button for transit
+
+document.getElementById('transitFilter').onclick = () => {
+	renderBlocks('1');
+	document.getElementById('transitFilter').classList.toggle('trains');
+  };
+  
+  function renderBlocks(requiresTransit) {
+	fetch('assets/data.json')
+	  .then(response => response.json()) // Parse the response as JSON
+	  .then(data => {
+		const container = document.getElementById('data-list');
+		container.innerHTML = ''; // Clear previous content
+	
+		data.forEach((block) => {
+		  if (block.requiresTransit === requiresTransit) {
+			// Add the block
+			let listItem =
+			  `
+				<li>
+				  <p>${block.emoji}</p>
+				  <h2>${block.itemName}</h2>
+				  <p>Suitable if you're broke?</p>
+				  <p>${block.costsMoney}</p>
+				  <p><em>Activity Rating</em></p>
+				  <p>${block.activityRating} / 10</p>
+				</li>
+			  `;
+	
+			container.insertAdjacentHTML('beforeend', listItem);
+		  }
+		});
+  });
+}
+
+  // Filter button for produtivity
+
+document.getElementById('productiveFilter').onclick = () => {
+	renderBlocks('1');
+	document.getElementById('productiveFilter').classList.toggle('potato');
+  };
+  
+  function renderBlocks(productive) {
+	fetch('assets/data.json')
+	  .then(response => response.json()) // Parse the response as JSON
+	  .then(data => {
+		const container = document.getElementById('data-list');
+		container.innerHTML = ''; // Clear previous content
+	
+		data.forEach((block) => {
+		  if (block.productive === productive) {
+			// Add the block
+			let listItem =
+			  `
+				<li>
+				  <p>${block.emoji}</p>
+				  <h2>${block.itemName}</h2>
+				  <p>Suitable if you're broke?</p>
+				  <p>${block.costsMoney}</p>
+				  <p><em>Do I have to leave the neighborhood?</em></p>
+				  <p>${block.requiresTransit}</p>
+				  <p><em>Activity Rating</em></p>
+				  <p>${block.activityRating} / 10</p>
+				</li>
+			  `;
+	
+			container.insertAdjacentHTML('beforeend', listItem);
+		  }
+		});
+	  })
+  }
